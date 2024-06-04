01e5ddd857ba501c4bc8bb6e24137c9a077ae2f2
Melanie <mho-hoffmann@web.de>
2024-06-04 20:04:06 +0200

    Change the apiKey

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "o2ftab814ad84766b140e87a80b1fa43";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
